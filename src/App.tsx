a4910e259ac8c81531c53f768f6756b81848363c
Yotam Zaken <yotam@gluework.io>
2023-09-21 16:50:58 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ListContainer from "./components/ListContainer";
+import { PlayerProvider } from "./player-context";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <PlayerProvider>
+        <div className="App">
+            <ListContainer />
+        </div>
+      </PlayerProvider>
   );
 }
 
